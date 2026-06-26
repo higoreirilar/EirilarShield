6d7f61148eb9527db27ab379538807276b3c45b1
higoreirilar <higoreirilar@gmail.com>
2026-06-26 10:55:27 -0300

    Update usuarios.py

--- routes/usuarios.py
+++ routes/usuarios.py
@@ -1,1 +1,21 @@
+from flask import Blueprint, render_template
+from flask_login import login_required
 
+from models import Usuario
+
+usuarios = Blueprint("usuarios", __name__)
+
+
+# =========================
+# LISTA DE USUÁRIOS
+# =========================
+@usuarios.route("/usuarios")
+@login_required
+def listar():
+
+    lista_usuarios = Usuario.query.order_by(Usuario.id.desc()).all()
+
+    return render_template(
+        "usuarios.html",
+        usuarios=lista_usuarios
+    )
